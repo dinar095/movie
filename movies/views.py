4d214e23ad7e82393b2d4292c192f35a33d4bdae
Desausag <dinar095@hotmail.com>
2020-12-23 18:36:07 +0300

    добавлено разделение страницы на части

--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {"movie_list": movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
 
-class MovieDetailView(View):
+class MovieDetailView(DetailView):
     """Полное описание фильма"""
-    def get(self, request, pk):
-        movie = Movie.objects.get(id=pk)
-        return render(request, "movies/movie_detail.html", {"movie":movie})
+    model = Movie
+    slug_field = "url"
